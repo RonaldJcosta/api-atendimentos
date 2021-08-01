237ea0111595e8bb5ba437436f214d857f896fab
Ronald <ronald.jc@hotmail.com>
2021-08-01 13:48:22 -0300

    Foi adicionado o nodemon

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 const app = express();
 
 const _PORT = 3000;
 app.listen(_PORT, () => {
 	console.log(`App running on port ${_PORT}`);
+});
+
+app.get('/atendimento', (req, res) => {
+	return res.send('Hello world');
 });
