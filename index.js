7ba79aa6b9557acb8ebf1098e47a9dbf4b0b89e0
Ronald <ronald.jc@hotmail.com>
2021-08-02 16:35:23 -0300

    Foi implementado as querys para se criar as tabelas do banco de dados

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const customExpress = require('./config/costumeExpress');
 const conexao = require('./infraestrutura/conexao');
-
+const Tabelas = require('./infraestrutura/tabelas');
 conexao.connect(err => {
 	if(err) {
 		console.log("Houve um erro ao se conectar com o banco de dados");
 	} else {
+
 		console.log("Banco de dados conectado com sucesso");
+		
+		Tabelas.init(conexao);
 		const app =  customExpress();
-		const POSRT = 3000;
+		const PORT = 3000;
+
 		app.listen(PORT, () => {
 			console.log(`Servidor rodando na porta ${PORT}` );
 		})
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,7 +1,20 @@
 class Tabelas {
 	init(conexao) {
-		console.log("Tabelas foram criadas");
-	}	
+		this.conexao = conexao;
+
+		this.criarTbAtendimentos();
+	}
+	
+		criarTbAtendimentos() {
+			const sql = "CREATE TABLE tb_atendimento(id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
+			this.conexao.query(sql, (err) => {
+				if(err) {
+					console.log("Houve um erro ao executar query "+err);
+				} else {
+					console.log("Tabela atendimento criada com sucesso !");
+				}
+			})
+		}
 }
 
 module.exports = new Tabelas;
