516a5dc65f307a62a0d8c9ac9d5c4ae5cf582fe7
Ronald <ronald.jc@hotmail.com>
2021-08-02 11:57:27 -0300

    Foi criada conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const customExpress = require('./config/costumeExpress');
+const conexao = require('./infraestrtura/conexao');
+
+try {
+	
+	conexao.connect(() => {
+		console.log("Foi realizado a conexão com sucesso");
+	});
+
+} catch(err) {
+	console.log("Houve um erro ao conectar ao banco de dados");
+}
 
 const app = customExpress();
 const _PORT = 3000;
