e7414fae077cb0ecde7e159a781c133fff038294
Ronald <ronald.jc@hotmail.com>
2021-08-03 14:21:57 -0300

    Foi feita a correção da condiçãopara se criar uma tabela

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -2,11 +2,11 @@ class Tabelas {
 	init(conexao) {
 		this.conexao = conexao;
 
-		//this.criarTbAtendimentos();
+	this.criarTbAtendimentos();
 	}
 	
 		criarTbAtendimentos() {
-			const sql = "CREATE TABLE IF NOT EXIST tb_atendimento(id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
+			const sql = "CREATE TABLE IF NOT EXISTS tb_atendimento(id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
 			this.conexao.query(sql, (err) => {
 				if(err) {
 					console.log("Houve um erro ao executar query "+err);
